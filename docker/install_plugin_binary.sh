a5e01c4a312ec28dc2b75401c11b1a9d8662d2fd
Jacob Wan <jacob@wildlemur.com>
2019-04-06 09:45:45 -0700

    Break long line in install_plugin_binary.sh

--- docker/install_plugin_binary.sh
+++ docker/install_plugin_binary.sh
@@ -2,6 +2,7 @@
 echo "Building the Terraform provider plugin binary..."
 make build
 mkdir -p /root/.terraform.d/plugins/linux_amd64
-cp /go/bin/terraform-provider-citrixitm /root/.terraform.d/plugins/linux_amd64/
+cp /go/bin/terraform-provider-citrixitm \
+/root/.terraform.d/plugins/linux_amd64/
 echo "Copied the plugin binary to the user's Terraform plugins directory."
 echo "You can now use the plugin inside the docker container after running 'terraform init' from within your root module directory."

d52d4e1254ebc8b9dfc791b116e135f7e9b9c51a
Jacob Wan <jacob@wildlemur.com>
2019-04-06 09:18:33 -0700

    Set exit code in start_container.sh properly

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -5,11 +5,11 @@ then
     if [ -z $(docker ps --filter=name='citrixitm_tf_dev_container' -a -q) ]
     then
         echo "The citrixitm_tf_dev_container container doesn't exist. Did you mean \"make docker-run\"?"
-        RC=1
+        exit 1
     else
         docker start -ai $1
     fi
 else
     echo 'The citrixitm_tf_dev_container container is already running. Did you mean "make docker-exec-bash"?'
-    RC=1
+    exit 1
 fi

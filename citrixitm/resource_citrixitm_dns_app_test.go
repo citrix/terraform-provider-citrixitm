49a088898eee80e72b44d6d300f0b480184f66cc
Jacob Wan <jacob@wildlemur.com>
2019-05-22 20:31:34 -0700

    Safety Dance requires valid app source in acceptance tests

--- citrixitm/resource_citrixitm_dns_app_test.go
+++ citrixitm/resource_citrixitm_dns_app_test.go
@@ -19,9 +19,21 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
+const minimalAppSource = `function init(config) {}
+
+function onRequest(request, response) {
+    response.addCName('foo.example.com');
+    response.setTTL(20);
+}
+`
+
+const updatedMinimalAppSource = minimalAppSource + "\n// foo"
+
 var (
-	appName        string
-	appNameUpdated string
+	appName                        string
+	appNameUpdated                 string
+	escapedMinimalAppSource        = strings.Replace(minimalAppSource, "\n", "\\n", -1)
+	escapedUpdatedMinimalAppSource = strings.Replace(updatedMinimalAppSource, "\n", "\\n", -1)
 )
 
 func init() {
@@ -78,7 +90,7 @@ func TestAccDnsApp_basic(t *testing.T) {
 						&testAccCitrixITMDnsAppExpectedAttributes{
 							Name:          appName,
 							Description:   "some description",
-							AppData:       "// some source",
+							AppData:       minimalAppSource,
 							FallbackCname: "fallback.foo.com",
 						}),
 				),
@@ -92,7 +104,7 @@ func TestAccDnsApp_basic(t *testing.T) {
 						&testAccCitrixITMDnsAppExpectedAttributes{
 							Name:          appNameUpdated,
 							Description:   "some description",
-							AppData:       "// some source",
+							AppData:       minimalAppSource,
 							FallbackCname: "fallback.foo.com",
 						}),
 				),
@@ -106,7 +118,7 @@ func TestAccDnsApp_basic(t *testing.T) {
 						&testAccCitrixITMDnsAppExpectedAttributes{
 							Name:          appNameUpdated,
 							Description:   "some description",
-							AppData:       "// some source foo",
+							AppData:       updatedMinimalAppSource,
 							FallbackCname: "fallback.foo.com",
 						}),
 				),
@@ -120,7 +132,7 @@ func TestAccDnsApp_basic(t *testing.T) {
 						&testAccCitrixITMDnsAppExpectedAttributes{
 							Name:          appNameUpdated,
 							Description:   "some description foo",
-							AppData:       "// some source foo",
+							AppData:       updatedMinimalAppSource,
 							FallbackCname: "fallback.foo.com",
 						}),
 				),
@@ -134,7 +146,7 @@ func TestAccDnsApp_basic(t *testing.T) {
 						&testAccCitrixITMDnsAppExpectedAttributes{
 							Name:          appNameUpdated,
 							Description:   "some description foo",
-							AppData:       "// some source foo",
+							AppData:       updatedMinimalAppSource,
 							FallbackCname: "fallback.bar.com",
 						}),
 				),
@@ -161,7 +173,9 @@ func testAccCheckCitrixITMDnsAppAttributes(got *itm.DNSApp, want *testAccCitrixI
 		if err = testValues("fallback CNAME", want.FallbackCname, got.FallbackCname); err != nil {
 			return
 		}
-		if err = testValues("app data", want.AppData, got.AppData); err != nil {
+		// App data is stripped of leading and trailing whitespace before being submitted to the API
+		trimmed := strings.TrimSpace(want.AppData)
+		if err = testValues("app data", trimmed, got.AppData); err != nil {
 			return
 		}
 		// Check the app CNAME
@@ -208,9 +222,9 @@ func testAccCheckCitrixITMDnsAppConfig(randString string) string {
 resource "citrixitm_dns_app" "foo" {
   name 				= "foo-%s"
   description		= "some description"
-  app_data			= "// some source"
+  app_data			= "%s"
   fallback_cname	= "fallback.foo.com"
-}`, randString)
+}`, randString, escapedMinimalAppSource)
 }
 
 func testAccCheckCitrixITMDnsAppConfig_Rename(randString string) string {
@@ -218,9 +232,9 @@ func testAccCheckCitrixITMDnsAppConfig_Rename(randString string) string {
 resource "citrixitm_dns_app" "foo" {
   name 				= "bar-%s"
   description		= "some description"
-  app_data			= "// some source"
+  app_data			= "%s"
   fallback_cname	= "fallback.foo.com"
-}`, randString)
+}`, randString, escapedMinimalAppSource)
 }
 
 func testAccCheckCitrixITMDnsAppConfig_ChangeAppData(randString string) string {
@@ -228,9 +242,9 @@ func testAccCheckCitrixITMDnsAppConfig_ChangeAppData(randString string) string {
 resource "citrixitm_dns_app" "foo" {
   name 				= "bar-%s"
   description		= "some description"
-  app_data			= "// some source foo"
+  app_data			= "%s"
   fallback_cname	= "fallback.foo.com"
-}`, randString)
+}`, randString, escapedUpdatedMinimalAppSource)
 }
 
 func testAccCheckCitrixITMDnsAppConfig_ChangeDescription(randString string) string {
@@ -238,9 +252,9 @@ func testAccCheckCitrixITMDnsAppConfig_ChangeDescription(randString string) stri
 resource "citrixitm_dns_app" "foo" {
   name 				= "bar-%s"
   description		= "some description foo"
-  app_data			= "// some source foo"
+  app_data			= "%s"
   fallback_cname	= "fallback.foo.com"
-}`, randString)
+}`, randString, escapedUpdatedMinimalAppSource)
 }
 
 func testAccCheckCitrixITMDnsAppConfig_ChangeFallbackCNAME(randString string) string {
@@ -248,9 +262,9 @@ func testAccCheckCitrixITMDnsAppConfig_ChangeFallbackCNAME(randString string) st
 resource "citrixitm_dns_app" "foo" {
   name 				= "bar-%s"
   description		= "some description foo"
-  app_data			= "// some source foo"
+  app_data			= "%s"
   fallback_cname	= "fallback.bar.com"
-}`, randString)
+}`, randString, escapedUpdatedMinimalAppSource)
 }
 
 // Test that the app is truly gone
